781673f593fee223a48363f3e03fca2292837165
Shalini M <114478851+ShaliniMahadeva@users.noreply.github.com>
2026-02-03 17:29:01 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error"
+    return a / b
+
+print("Add:", add(10, 5))
+print("Subtract:", subtract(10, 5))
+print("Multiply:", multiply(10, 5))
+print("Divide:", divide(10, 5))
